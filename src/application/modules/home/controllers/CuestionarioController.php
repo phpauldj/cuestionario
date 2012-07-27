dc042826b364427e4f8a3b2aa3dc129ca4d017c4
juan kuga <juankp3@gmail.com>
2012-07-26 23:25:24 -0500

    cuestionariocontroller limpio

--- src/application/modules/home/controllers/CuestionarioController.php
+++ src/application/modules/home/controllers/CuestionarioController.php
@@ -26,14 +26,9 @@ class Home_CuestionarioController extends App_Controller_Action_Encuestado{
         $p = new Application_Model_Pregunta();
         $listaPregunta = $p->getPregunta(1);
         
-        //var_dump($listaPregunta);
-        //$paginador = new Zend_Paginator(new Zend_Paginator_Adapter_Array($listaPregunta));
         $paginador = Zend_Paginator::factory($listaPregunta);
         $paginador->setItemCountPerPage($preguntaForPage)->setCurrentPageNumber($page);
         $this->view->listaPreguntas = $paginador;
-//        $this->_helper->layout->disableLayout();
-//        $this->_helper->viewRenderer->setNoRender();
-        
     }
     
      public function preguntasAction()
@@ -49,24 +44,11 @@ class Home_CuestionarioController extends App_Controller_Action_Encuestado{
         $p = new Application_Model_Pregunta();
         $listaPregunta = $p->getPregunta(1);
         
-        //var_dump($listaPregunta);
-        //$paginador = new Zend_Paginator(new Zend_Paginator_Adapter_Array($listaPregunta));
+        
         $paginador = Zend_Paginator::factory($listaPregunta);
         $paginador->setItemCountPerPage($preguntaForPage)->setCurrentPageNumber($page);
         $this->view->listaPreguntas = $paginador;
-//        $this->_helper->layout->disableLayout();
-//        $this->_helper->viewRenderer->setNoRender();
-       
-//            if ($this->getRequest()->isPost()) {
-//                
-//                echo "yaaa";
-//            $params = $this->_getAllParams();
-//            
-//            echo "<pre>";
-//            print_r($params);
-//            echo "</pre>";
-//
-//        }
+
     }
     
     public function  agregarAction()
@@ -81,40 +63,22 @@ class Home_CuestionarioController extends App_Controller_Action_Encuestado{
             unset($params['module']);
             unset($params['page']);
             
-//            echo "==>".$page;
-//            echo "<pre>";
-//            print_r($params);
-//            echo "</pre>";
-            
-           
-            
             $p = new Application_Model_Pregunta();
            
              foreach ($params as $key => $value) {
                  $op = $p->getRespuesta($key);
-                 //echo "<br>-> ".$p->getRespuesta($key);
+                 
                  if (empty($op)){
                      $data['opcion_id'] = $value;
                      $data['pregunta_id'] = $key;
                      $p->addOpcion($data, $this->auth['usuario']->id);
-                   // echo "<br>inset";
-                 }else{
+                 } else {
                       $data['opcion_id'] = $value;
                       $p->updateOpcion($data, $key, $this->auth['usuario']->id);
-                    //echo "<br>update"; 
                  }
-//                 
-                //echo "<br>".$key;   
             }
-            //$p->addOpcion($data);
-            
             $this->_redirect('/cuestionario/preguntas/page/'.$page);
-            //exit;
-//            if ($formAgregar->isValid($params)) {
-//                $c->agregarCliente($params);
-//                $this->getMessenger()->success('El cliente se agrego con exito!');
-//                $this->_redirect('/admin/clientes');
-//            }
+
         }
     }
 }
